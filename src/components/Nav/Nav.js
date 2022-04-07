b3d75cec45a8437284247c9a273be179d5d628f0
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-08 02:29:52 +0600

    added components

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.scss";
+
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <div class="Nav__title">
+        hello world
+      </div>
+      
+    </div>
+  );
+}
+
+export default Nav;
